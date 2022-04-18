a502085e83a214337d4eadc1c533aa09fda0b6fb
Klvst3r <klvstr3r@gmail.com>
2022-04-17 22:36:27 -0500

    Mostrar detalle del usuario en UserControler con metodo show

--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,8 @@ Route::get('/usuarios/nuevo', function(){
    return 'Crear unuevo usuario';
 });
 
-Route::get('/usuarios/{id}', function($id){
-   return "Mostrando detalles del usuario: {$id}";
-    })->where('id', '[0-9]+');   
+Route::get('/usuarios/{id}', 'UserController@show')
+    ->where('id', '[0-9]+');   
 
 /*Route::get('/saludo/{name}/{nickname}', function($name, $nickname){
     return "Bienvenido {$name}, tu nombre clave es: {$nickname}";
