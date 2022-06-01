39cb0d1aaf0f1ce2b73980b21d93d4442070a637
Klvst3r <klvst3r@gmail.com>
2022-06-01 12:35:13 -0500

    Completos los metodos up y down de la migración add_proffesion_to_users

--- database/migrations/2022_06_01_172104_add_profession_to_users.php
+++ database/migrations/2022_06_01_172104_add_profession_to_users.php
@@ -13,7 +13,9 @@ class AddProfessionToUsers extends Migration
      */
     public function up()
     {
-        //
+       Schema::table('users', function (Blueprint $table)){
+        $table->string('profession', 50)->nulltable();
+       }
     }
 
     /**
@@ -23,6 +25,8 @@ class AddProfessionToUsers extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table)){
+            $table->dropColumn('profession');
+        }
     }
 }
