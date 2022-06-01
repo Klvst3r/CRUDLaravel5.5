cfe956faa982f52b3da7dea4169836af20c339a7
Klvst3r <klvst3r@gmail.com>
2022-06-01 12:45:25 -0500

    Ágregar el campo profession despues del campo password

--- database/migrations/2022_06_01_172104_add_profession_to_users.php
+++ database/migrations/2022_06_01_172104_add_profession_to_users.php
@@ -14,7 +14,7 @@ class AddProfessionToUsers extends Migration
     public function up()
     {
        Schema::table('users', function (Blueprint $table){
-        $table->string('profession', 50)->nulltable();
+        $table->string('profession', 50)->nulltable()->after('password');
        });
     }
 
