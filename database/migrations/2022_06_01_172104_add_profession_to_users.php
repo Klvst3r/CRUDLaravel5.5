2d66b28503d604d24b798859bcd06d1ff0c6e25a
Klvst3r <klvst3r@gmail.com>
2022-06-01 13:02:36 -0500

    26. Modificar tablas existentes con las ligracioens de Laravel

--- database/migrations/2022_06_01_172104_add_profession_to_users.php
+++ database/migrations/2022_06_01_172104_add_profession_to_users.php
@@ -14,7 +14,7 @@ class AddProfessionToUsers extends Migration
     public function up()
     {
        Schema::table('users', function (Blueprint $table){
-        $table->string('profession', 50)->nulltable()->after('password');
+        $table->string('profession', 50)->nullable()->after('password');
        });
     }
 
