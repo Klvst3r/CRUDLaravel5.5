9ff639ae1eddf377eb8bf04c63d82be7c8cf007e
Klvst3r <klvst3r@gmail.com>
2022-06-01 12:39:19 -0500

    Se correige la migracion add_profesion_to_users

--- database/migrations/2022_06_01_172104_add_profession_to_users.php
+++ database/migrations/2022_06_01_172104_add_profession_to_users.php
@@ -13,9 +13,9 @@ class AddProfessionToUsers extends Migration
      */
     public function up()
     {
-       Schema::table('users', function (Blueprint $table)){
+       Schema::table('users', function (Blueprint $table){
         $table->string('profession', 50)->nulltable();
-       }
+       });
     }
 
     /**
@@ -25,8 +25,8 @@ class AddProfessionToUsers extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table)){
+        Schema::table('users', function(Blueprint $table){
             $table->dropColumn('profession');
-        }
+        });
     }
 }
